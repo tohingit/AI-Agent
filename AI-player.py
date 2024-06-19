c3b12a148c3060d3f84eebc6123e7575f8178eab
Anton <tiktoktoha2@gmail.com>
2024-06-19 11:00:16 +0300

    Create AI-player.py

---
+++ AI-player.py
@@ -0,0 +1,43 @@
+import math
+
+class AIPlayer:
+    def __init__(self, letter):
+        self.letter = letter
+
+    def get_move(self, game):
+        if len(game.available_moves()) == 9:
+            square = (0, 0)
+        else:
+            square = self.minimax(game, self.letter)['position']
+        return square
+
+    def minimax(self, state, player):
+        max_player = self.letter
+        other_player = 'O' if player == 'X' else 'X'
+
+        if state.current_winner == other_player:
+            return {'position': None, 'score': 1 * (state.num_empty_squares() + 1) if other_player == max_player else -1 * (state.num_empty_squares() + 1)}
+        elif not state.empty_squares():
+            return {'position': None, 'score': 0}
+
+        if player == max_player:
+            best = {'position': None, 'score': -math.inf}
+        else:
+            best = {'position': None, 'score': math.inf}
+
+        for possible_move in state.available_moves():
+            state.make_move(possible_move, player)
+            sim_score = self.minimax(state, other_player)
+
+            state.board[possible_move[0]][possible_move[1]] = ' '
+            state.current_winner = None
+            sim_score['position'] = possible_move
+
+            if player == max_player:
+                if sim_score['score'] > best['score']:
+                    best = sim_score
+            else:
+                if sim_score['score'] < best['score']:
+                    best = sim_score
+
+        return best
